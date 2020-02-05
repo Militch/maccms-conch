20d8c8e16c0051bcaeab17f84873d0f6cf90406d
Militch <litch.me@gmail.com>
2020-02-06 03:36:50 +0800

    fix

--- docker-maccms-conch-entrypoint.sh
+++ docker-maccms-conch-entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-set -e
 
 cd /var/www/html
 sed -i "s/\([']template_dir['][^']*\)['][^']*[']\(.*\)/\1'conch'\2/" ./application/extra/maccms.php
-
 sed -i "s/\([']mob_template_dir['][^']*\)['][^']*[']\(.*\)/\1'conch'\2/" ./application/extra/maccms.php
-
-has_old=$(cat ./application/data/config/quickmenu.txt | grep "[^/]*[^/]/*/admin/conch/theme")
+has_old=$(cat /var/www/html/application/data/config/quickmenu.txt | grep "[^/]*[^/]/*/admin/conch/theme")
 if [ -z "$has_old" ]; then
-    echo "" >> ./application/data/config/quickmenu.txt; \
-    echo "海螺主题设置,/${MACCMS_ADMIN_FILENAME}/admin/conch/theme\c" >> ./application/data/config/quickmenu.txt
+    echo "" >> ./application/data/config/quickmenu.txt
+    echo -e "海螺主题设置,/${MACCMS_ADMIN_FILENAME}/admin/conch/theme\c" >> ./application/data/config/quickmenu.txt
 fi
 
 source docker-maccms-entrypoint.sh
