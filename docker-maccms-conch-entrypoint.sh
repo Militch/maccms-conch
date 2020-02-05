ef00a5eeb2a64033798aefa4a0e08884e2cfe789
Militch <litch.me@gmail.com>
2020-02-06 02:53:42 +0800

    fix(script): fix entirypoint

--- docker-maccms-conch-entrypoint.sh
+++ docker-maccms-conch-entrypoint.sh
@@ -5,10 +5,10 @@ sed -i "s/\([']template_dir['][^']*\)['][^']*[']\(.*\)/\1'conch'\2/" /var/www/ht
 
 sed -i "s/\([']mob_template_dir['][^']*\)['][^']*[']\(.*\)/\1'conch'\2/" /var/www/html/application/extra/maccms.php
 
-echo "海螺主题设置,/${MACCMS_ADMIN_FILENAME}/admin/conch/theme"
-cat >> /var/www/html/application/data/config/quickmenu.txt <<EOF
-
-海螺主题设置,/${MACCMS_ADMIN_FILENAME}/admin/conch/theme
-EOF
+has_old=$(cat README.md | grep "[^/]*[^/]/*/admin/conch/theme")
+if [ -z "$has_old" ]; then
+    echo "" >> README.md; \
+    echo "海螺主题设置,/${MACCMS_ADMIN_FILENAME}/admin/conch/theme\c" >> README.md
+fi
 
 source docker-maccms-entrypoint.sh
