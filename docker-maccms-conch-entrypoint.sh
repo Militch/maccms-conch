5ec4bdd49c82e95f4b0a3c0c2b30fd157e9dc6c5
Militch <litch.me@gmail.com>
2020-02-06 02:59:18 +0800

    fix(script): change workspace

--- docker-maccms-conch-entrypoint.sh
+++ docker-maccms-conch-entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -e
 
-sed -i "s/\([']template_dir['][^']*\)['][^']*[']\(.*\)/\1'conch'\2/" /var/www/html/application/extra/maccms.php
+cd /var/www/html
+sed -i "s/\([']template_dir['][^']*\)['][^']*[']\(.*\)/\1'conch'\2/" ./application/extra/maccms.php
 
-sed -i "s/\([']mob_template_dir['][^']*\)['][^']*[']\(.*\)/\1'conch'\2/" /var/www/html/application/extra/maccms.php
+sed -i "s/\([']mob_template_dir['][^']*\)['][^']*[']\(.*\)/\1'conch'\2/" ./application/extra/maccms.php
 
-has_old=$(cat README.md | grep "[^/]*[^/]/*/admin/conch/theme")
+has_old=$(cat ./application/data/config/quickmenu.txt | grep "[^/]*[^/]/*/admin/conch/theme")
 if [ -z "$has_old" ]; then
-    echo "" >> README.md; \
-    echo "海螺主题设置,/${MACCMS_ADMIN_FILENAME}/admin/conch/theme\c" >> README.md
+    echo "" >> ./application/data/config/quickmenu.txt; \
+    echo "海螺主题设置,/${MACCMS_ADMIN_FILENAME}/admin/conch/theme\c" >> ./application/data/config/quickmenu.txt
 fi
 
 source docker-maccms-entrypoint.sh
